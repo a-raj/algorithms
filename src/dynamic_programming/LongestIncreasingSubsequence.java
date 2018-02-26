4c101464c496178c50a514fa31ddb66d90418496
a-raj <seriouswithavi@gmail.com>
2018-02-27 01:56:14 +0530

    add Longest Increasing Subsequence problem

---
+++ src/dynamic_programming/LongestIncreasingSubsequence.java
@@ -0,0 +1,34 @@
+package dynamic_programming;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubsequence {
+    public static void main(String[] args) {
+        int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 };
+
+        long time1 = System.currentTimeMillis();
+        System.out.println(find_lis(arr));
+        System.out.println(System.currentTimeMillis() - time1);
+    }
+
+    private static int find_lis(int[] arr) {
+        int n = arr.length;
+        int[] lis = new int[n];
+
+        Arrays.fill(lis, 1);
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i] > arr[j] && lis[i] < lis[j] + 1) {
+                    lis[i] = lis[j] + 1;
+                }
+            }
+        }
+        int max = 1;
+        for (int l : lis)
+            if ( max < l )
+                max = l;
+
+        return max;
+    }
+}
