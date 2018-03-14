0bfeb9f484d606cec70176870e4f8815862d2e9b
a-raj <seriouswithavi@gmail.com>
2018-03-14 12:49:24 +0530

    Graph and BFS

---
+++ src/trees/graph/BFS.java
@@ -0,0 +1,44 @@
+package trees.graph;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+
+    public static void main(String[] args) {
+        Graph_AdjacencyList graph = new Graph_AdjacencyList(4);
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 0);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 3);
+
+        bfs(2, graph, 4);
+    }
+
+    static void bfs(int s, Graph_AdjacencyList graph, final int v) {
+
+        boolean[] visited = new boolean[v];
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited[s] = true;
+        queue.add(s);
+
+        while ( !queue.isEmpty() ) {
+            s = queue.poll();
+            System.out.printf(s + " ");
+
+            for (Integer n : graph.getAdjacencyList()[s]) {
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+
+            }
+        }
+    }
+}
---
+++ src/trees/graph/Graph_AdjacencyList.java
@@ -0,0 +1,29 @@
+package trees.graph;
+
+import java.util.LinkedList;
+
+public class Graph_AdjacencyList {
+
+    private int v; //No of vertices
+    private LinkedList<Integer>[] adjacencyList;
+
+    public Graph_AdjacencyList(int v) {
+        this.v = v;
+        adjacencyList = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
+            adjacencyList[i] = new LinkedList();
+        }
+    }
+
+    public void addEdge(int v, int w) {
+        adjacencyList[v].add(w);
+    }
+
+    public int getV() {
+        return v;
+    }
+
+    public LinkedList<Integer>[] getAdjacencyList() {
+        return adjacencyList;
+    }
+}
